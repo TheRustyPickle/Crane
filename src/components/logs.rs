e1356d63eceda225690828ca978637a603ebe3f0
Rusty Pickle <rusty.pickle94@gmail.com>
2025-10-21 18:21:08 +0600

    Add log page

---
+++ src/components/logs.rs
@@ -0,0 +1,43 @@
+use iced::widget::scrollable::Scrollbar;
+use iced::widget::{column, container, scrollable, text};
+use iced::{Alignment, Color, Element, Length, Padding};
+
+use crate::icon::left_arrow;
+use crate::utils::{mono, primary_button};
+use crate::{MainWindow, Message};
+
+impl MainWindow {
+    pub fn log_page(&self) -> Element<'_, Message> {
+        let back_button = primary_button(
+            left_arrow()
+                .align_y(Alignment::Center)
+                .align_x(Alignment::Center)
+                .style(|_| text::Style {
+                    color: Some(Color::WHITE),
+                }),
+        )
+        .on_press(Message::ShowCrates)
+        .width(50)
+        .padding(5);
+
+        let mut log_text = self.logs.join("\n");
+
+        if log_text.is_empty() {
+            log_text = "No logs to show".to_string();
+        }
+
+        let logs = text(log_text).font(mono()).size(15);
+
+        let log_layout = column![back_button, logs].padding(5).spacing(10);
+
+        container(
+            scrollable(log_layout).direction(scrollable::Direction::Vertical(Scrollbar::new())),
+        )
+        .padding(Padding {
+            right: 5.0,
+            ..Default::default()
+        })
+        .height(Length::Fill)
+        .into()
+    }
+}
