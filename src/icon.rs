63d35829c3fd71bbed71a9504925aa1d0c16bfcf
Rusty Pickle <rusty.pickle94@gmail.com>
2025-10-22 20:42:03 +0600

    Add new icno

--- src/icon.rs
+++ src/icon.rs
@@ -1,10 +1,14 @@
 // Generated automatically by iced_fontello at build time.
 // Do not edit manually. Source: ../fonts/icons.toml
-// 9a33e771372b330754708d9074f052eab5aaa77cfc7ab847e60f8f2ffb2da17b
+// 244f4a7eb0319f009cc94857a20f10eaa01a0c6690f3c452871d6bcc01ec5ed8
 use iced::Font;
 use iced::widget::{Text, text};
 
 pub const FONT: &[u8] = include_bytes!("../fonts/icons.ttf");
+
+pub fn github<'a>() -> Text<'a> {
+    icon("\u{F307}")
+}
 
 pub fn left_arrow<'a>() -> Text<'a> {
     icon("\u{E00D}")
